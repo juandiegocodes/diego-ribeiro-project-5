3edace43c1c438fc4a9567870506962406acd112
Diego Ribeiro <rfdiego91@gmail.com>
2019-09-10 11:28:45 -0400

    Updates

---
+++ precache-manifest.9ad8601b33a8372a079bf8c1688f47db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323e6e2a946d83ed798e22850ef24037",
+    "url": "/diego-ribeiro-project-5/index.html"
+  },
+  {
+    "revision": "e695428410d90c4cfb1c",
+    "url": "/diego-ribeiro-project-5/static/css/main.d7372b39.chunk.css"
+  },
+  {
+    "revision": "cf9263faad574058bb30",
+    "url": "/diego-ribeiro-project-5/static/js/2.321ec951.chunk.js"
+  },
+  {
+    "revision": "e695428410d90c4cfb1c",
+    "url": "/diego-ribeiro-project-5/static/js/main.c271087a.chunk.js"
+  },
+  {
+    "revision": "df2a26fd80666e6e8ffb",
+    "url": "/diego-ribeiro-project-5/static/js/runtime~main.54a2ab41.js"
+  },
+  {
+    "revision": "f60c31a70ee98012cc25e62f009fe108",
+    "url": "/diego-ribeiro-project-5/static/media/tableBackground.f60c31a7.png"
+  }
+]);
